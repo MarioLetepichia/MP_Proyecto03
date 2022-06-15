d56bd5c487cf831afb2573943bf34474ab41bfb7
Ivette612 <you@example.com>
2022-06-15 18:24:14 -0500

    clase para descentriptar completa

--- src/AESFileEncryption.java
+++ src/AESFileEncryption.java
@@ -12,18 +12,30 @@ import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 
 public class AESFileEncryption {
-	public static void main(String[] args) throws Exception {
 
-		// file to be encrypted
-		FileInputStream inFile = new FileInputStream("prueba.txt");
+    /*
+     * Método para obtener la contraseña segura apartir el string que 
+     * se le pide al usuario
+     */
+    public static SecretKey getSecretKey(String password){
+
+        SecretKeyFactory factory = SecretKeyFactory
+        .getInstance("PBKDF2WithHmacSHA1");
+        KeySpec keySpec = new PBEKeySpec(password.toCharArray(), salt, 65536,
+        256);
+        SecretKey secretKey = factory.generateSecret(keySpec);
+        SecretKey secret = new SecretKeySpec(secretKey.getEncoded(), "AES");
+        return secret;
+
+    }
+
+    public static void encript(FileInputStream inFile, SecretKey secret){
+        // file to be encrypted
+		inFile = new FileInputStream("prueba2.txt");
 
 		// encrypted file
 		FileOutputStream outFile = new FileOutputStream("encryptedfile.des");
-
-		// password to encrypt the file
-		String password = "javapapers";
-
-		// password, iv and salt should be transferred to the other end
+        	// password, iv and salt should be transferred to the other end
 		// in a secure manner
 
 		// salt is used for encoding
@@ -36,15 +48,7 @@ public class AESFileEncryption {
 		FileOutputStream saltOutFile = new FileOutputStream("salt.enc");
 		saltOutFile.write(salt);
 		saltOutFile.close();
-
-		SecretKeyFactory factory = SecretKeyFactory
-				.getInstance("PBKDF2WithHmacSHA1");
-		KeySpec keySpec = new PBEKeySpec(password.toCharArray(), salt, 65536,
-				256);
-		SecretKey secretKey = factory.generateSecret(keySpec);
-		SecretKey secret = new SecretKeySpec(secretKey.getEncoded(), "AES");
-
-		//
+        	//
 		Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
 		cipher.init(Cipher.ENCRYPT_MODE, secret);
 		AlgorithmParameters params = cipher.getParameters();
@@ -78,6 +82,54 @@ public class AESFileEncryption {
 
 		System.out.println("File Encrypted.");
 		
-	}
+		}
+
+        /**
+         * Método para descencrptar apartir de la key
+         * @param secret
+         */
+        public static void descript(SecretKey secret){
+        // reading the salt
+		// user should have secure mechanism to transfer the
+		// salt, iv and password to the recipient
+		FileInputStream saltFis = new FileInputStream("salt.enc");
+		byte[] salt = new byte[8];
+		saltFis.read(salt);
+		saltFis.close();
+
+		// reading the iv
+		FileInputStream ivFis = new FileInputStream("iv.enc");
+		byte[] iv = new byte[16];
+		ivFis.read(iv);
+		ivFis.close();
+
+		// file decryption
+		Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+		cipher.init(Cipher.DECRYPT_MODE, secret, new IvParameterSpec(iv));
+		FileInputStream fis = new FileInputStream("encryptedfile.des");
+		FileOutputStream fos = new FileOutputStream("plainfile_decrypted.txt");
+		byte[] in = new byte[64];
+		int read;
+		while ((read = fis.read(in)) != -1) {
+			byte[] output = cipher.update(in, 0, read);
+			if (output != null)
+				fos.write(output);
+		}
+
+		byte[] output = cipher.doFinal();
+		if (output != null)
+			fos.write(output);
+		fis.close();
+		fos.flush();
+		fos.close();
+		System.out.println("File Decrypted.");
+	
+        }
+	public static void main(String[] args) throws Exception {
+
+        SecretKey secret= getSecretKey("password");
+        descript(secret);
+
+    }
 
 }
