ab278ee535d464752152c13203f3af47655fb87c
Aislinn-Liahut <liahutaislinn@ciencias.unam.mx>
2022-06-15 21:59:14 -0500

    main password sin eco

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 /**
  * Clase principal del programa
  */
-
+import java.io.*;
 public class Main{
 
-    public static void prueba(String nombreArchivo,int n,int t,String documentoClaro){
+    public static void prueba(String nombreArchivo,int n,int t,String documentoClaro,String contrasenia){
         System.out.println("cifrando");
     }
 
@@ -21,7 +21,8 @@ public class Main{
            int int1 = Integer.parseInt(args[2]);
            int int2 = Integer.parseInt(args[3]);
            String nombre2 = args[4];
-           prueba(nombre1,int1,int2,nombre2);
+           String contrasenia= args[5];
+           prueba(nombre1,int1,int2,nombre2,contrasenia);
           
         }
         if(opcion == "d"){
@@ -30,6 +31,26 @@ public class Main{
             prueba2(nombre1,nombre2);
         }
 
+        // Create the console object
+        Console cnsl= System.console();
+  
+        if (cnsl == null) {
+            System.out.println(
+                "No console available");
+            return;
+        }
+  
+    
+        // Read password
+        // into character array
+        char[] ch = cnsl.readPassword(
+            "Teclea la llave : ");
+        String password = new String(ch);
+        // Print password
+        /*  System.out.println(
+             "llave : " + password); */
+
+      
 
     }
 }
